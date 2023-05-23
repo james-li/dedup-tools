49135d9902996d58d253d4a5d45ffb9571d2812f
James Li <guoguopapa@gmail.com>
2023-05-24 03:04:41 +0800

    fix async io dedup action

--- dedup_utils.py
+++ dedup_utils.py
@@ -31,7 +31,7 @@ class dedup_utils(object):
         if self.helper:
             await self.helper.info(message)
 
-    async def dedup_files_by_size(self) -> dict:
+    def dedup_files_by_size(self) -> dict:
         files_info = {}
         for root, dirs, files in os.walk(self.directory):
             for file in files:
@@ -43,13 +43,15 @@ class dedup_utils(object):
         return {x: files_info[x] for x in files_info if len(files_info[x]) > 1}
 
     async def get_file_md5_by_chunk(self, file_path: str, chunk_size=65536):
+        md5_hash = hashlib.md5()
         async with aiofiles.open(file_path, "rb") as fp:
             while True:
                 chunk = await fp.read(chunk_size)
                 if not chunk:
                     break
-                await self.set_info(file_path)
-                yield hashlib.md5(chunk).hexdigest()
+                # await self.set_info(file_path)
+                md5_hash.update(chunk)
+                yield md5_hash.hexdigest()
 
     async def get_chunk_md5(self, file_path, md5_iter, dedup_files_by_chunk_md5: dict, lock):
         md5 = await next(md5_iter)
@@ -69,15 +71,18 @@ class dedup_utils(object):
                     if len(files) > 1:
                         done = False
                         new_md5_files = {}
-                        tasks = []
-                        for file, md5_iter in files:
-                            tasks.append(asyncio.create_task(self.get_chunk_md5(file, md5_iter, new_md5_files, lock)))
-                        await asyncio.gather(*tasks)
+                        # tasks = []
+                        for file_path, md5_iter in files:
+                            md5 = await md5_iter.__anext__()
+                            async with lock:
+                                if md5 not in new_md5_files:
+                                    new_md5_files[md5] = []
+                                new_md5_files[md5].append((file_path, md5_iter))
                         new_files_list.extend([new_md5_files[x] for x in new_md5_files if len(new_md5_files[x]) > 1])
                 dedup_files_list_tmp = new_files_list
                 if done:
                     break
-            except TypeError as e:
+            except StopAsyncIteration as e:
                 break
             except BaseException as e:
                 traceback.print_exc()
@@ -89,7 +94,7 @@ class dedup_utils(object):
         return dedup_files_list
 
     async def dedup_files_in_directory(self):
-        files_by_size = await self.dedup_files_by_size()
+        files_by_size = self.dedup_files_by_size()
         for size, files in files_by_size.items():
             file_list = await self.dedup_file_list_by_md5(files)
             for l in file_list:
@@ -109,6 +114,16 @@ async def main(directory: str):
     dutils = dedup_utils(directory, dedup_process_helper())
     async for files in dutils.dedup_files_in_directory():
         print(files)
+    # files = dutils.dedup_files_by_size()
+    # file_path = files[[key for key in files.keys() if key > 32768][0]][0]
+    # md5_iter = dutils.get_file_md5_by_chunk(file_path, 32768)
+    # while True:
+    #     try:
+    #         md5 = await md5_iter.__anext__()
+    #         print(md5)
+    #     except BaseException as e:
+    #         traceback.print_exc()
+    #         break
 
 
 if __name__ == "__main__":
